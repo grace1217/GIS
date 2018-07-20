acab017b7e00c5bccb1ed62cc46f684c16388b3e
grace1217 <youdexiaowu@qq.com>
2018-07-20 14:13:16 +0800

    新增GIS

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import './App.css';
+
 import Home from './Component/Home'
 import Trag from './Component/Trag'
 import Thermo from './Component/Thermo'
 
-class App extends Component{
-  render(){
-    return(
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">飞行图</Link>
-            </li>
-            <li>
-              <Link to="/traj">行走轨迹</Link>
-            </li>
-            <li>
-              <Link to="/thermo">热力图</Link>
-            </li>
-          </ul>
 
-          <hr />
 
-          <Route exact path="/" component={Home} />
-          <Route path="/traj" component={Trag} />
-          <Route path="/thermo" component={Thermo} />
-        </div>
-      </Router>
-    )
-  }
+class App extends Component {  
+    
+
+    render() {
+        return (
+             <Router>
+                <div>
+                  <ul>
+                    <li>
+                      <Link to="/">飞行图</Link>
+                    </li>
+                    <li>
+                      <Link to="/traj">行走轨迹</Link>
+                    </li>
+                    <li>
+                      <Link to="/thermo">热力图</Link>
+                    </li>
+                  </ul>
+
+                  <hr />
+                  <Route exact path="/" component={Home} />
+                  <Route path="/traj" component={Trag} />
+                  <Route path="/thermo" component={Thermo} />
+
+                  
+                </div>
+              </Router>
+        );
+    }
 }
 
-
-
-export default App;
+export default App
